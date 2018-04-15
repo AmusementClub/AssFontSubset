0db0a28f040a608cd24467c2732e0ffaa1ba1a31
youlun <youlun@me.com>
2018-04-16 04:46:55 +0800

    v1.0

---
+++ AssFontSubset/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace AssFontSubset
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 303);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(867, 176);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 30);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "字幕文件";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 218);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "字体目录";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 262);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "输出目录";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(115, 215);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(767, 32);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(115, 259);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(767, 32);
+            this.textBox3.TabIndex = 6;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(112, 27);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(767, 179);
+            this.listBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 494);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ AssFontSubset/Form1.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace AssFontSubset
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        struct SubsetFontInfo
+        {
+            public string OriginalFontFile;
+            public string SubsetFontFile;
+            public string FontNameInAss;
+            public string SubsetFontName;
+            public int TrackIndex;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] assFiles = this.listBox1.Items.Cast<string>().ToArray();
+            string fontFolder = this.textBox2.Text;
+            string outputFolder = this.textBox3.Text;
+
+            if (assFiles.Length == 0) {
+                MessageBox.Show("没有设置字幕文件");
+                return;
+            }
+            if (!Directory.Exists(fontFolder)) {
+                MessageBox.Show("字体目录不存在");
+                return;
+            }
+            if (!Directory.Exists(outputFolder)) {
+                Directory.CreateDirectory(outputFolder);
+            }
+
+
+            // Dictionary<字体名, List<Tuple<ASS文件名, 行数>>>
+            Dictionary<string, List<Tuple<string, int>>> fonts = new Dictionary<string, List<Tuple<string, int>>>();
+
+            //Dictionary<字体名, 字符>
+            Dictionary<string, string> texts = new Dictionary<string, string>();
+
+            foreach (string assFile in assFiles) {
+                AssParser parser = new AssParser();
+                var result = parser.Parse(assFile);
+                var _fonts = result.Item1;
+                var _texts = result.Item2;
+
+                foreach (var font in _fonts) {
+                    string fontName = font.Key;
+                    if (!fonts.ContainsKey(fontName)) {
+                        fonts[fontName] = new List<Tuple<string, int>>();
+                    }
+                    foreach (int line in font.Value) {
+                        fonts[fontName].Add(Tuple.Create(assFile, line));
+                    }
+                }
+
+                foreach (var text in _texts) {
+                    string fontName = text.Key;
+                    if (!texts.ContainsKey(fontName)) {
+                        texts[fontName] = "";
+                    }
+                    texts[fontName] += text.Value;
+                }
+            }
+            var keys = new List<string>(texts.Keys);
+            foreach (var key in keys) {
+                texts[key] = new string(texts[key].Distinct().ToArray());
+            }
+
+
+            var subsetFonts = new List<SubsetFontInfo>();
+
+
+            // Dictionary<字体名, List<Tuple<TTC字体轨道号, 文件名>>>
+            var fontInfo = new Dictionary<string, List<Tuple<int, string>>>();
+
+            var fontFiles = Directory.EnumerateFiles(fontFolder, "*.*", SearchOption.TopDirectoryOnly);
+            foreach (var file in fontFiles) {
+                if (Path.GetExtension(file) == ".txt") {
+                    continue;
+                }
+                PrivateFontCollection collection = new PrivateFontCollection();
+                collection.AddFontFile(file);
+                if (collection.Families.Length < 1) {
+                    throw new Exception("字体目录存在多余物或无法读取字体名称。文件名：" + file);
+                }
+
+                int index = -1;
+                string fontName = string.Empty;
+                if (collection.Families.Length > 1) {
+                    var fontFamilies = System.Windows.Media.Fonts.GetFontFamilies(file).ToList();
+                    for (index = 0; index < fontFamilies.Count; index++) {
+                        var result = fontFamilies[index].FamilyNames.Values.Where(name => fonts.ContainsKey(name));
+                        if (result.Count() < 1) {
+                            continue;
+                        }
+                        fontName = result.First();
+                        break;
+                    }
+                } else if (collection.Families.Length == 1) {
+                    fontName = collection.Families[0].Name;
+                } else {
+                    continue;
+                }
+
+                if (!fontInfo.ContainsKey(fontName)) {
+                    fontInfo[fontName] = new List<Tuple<int, string>>();
+                }
+                fontInfo[fontName].Add(Tuple.Create(index, file));
+            }
+
+            foreach (var text in texts) {
+                var fontName = text.Key;
+                var characters = text.Value;
+
+                var tmpfile = $@"{fontFolder}\{fontName}.txt";
+                using (StreamWriter sw = new StreamWriter(tmpfile, false, new UTF8Encoding(false))) {
+                    sw.Write(characters);
+                }
+
+                if (!fontInfo.ContainsKey(fontName)) {
+                    throw new Exception("字体未找到！" + fontName);
+                }
+
+                string randomName = this.RandomString(8);
+
+                foreach (var tuple in fontInfo[fontName]) {
+                    int index = tuple.Item1;
+                    string fontFile = tuple.Item2;
+
+                    string outputFile = string.Empty;
+                    if (fontFile.EndsWith(".ttc")) {
+                        outputFile = $"{outputFolder}\\{Path.GetFileNameWithoutExtension(fontFile)}.ttf";
+                    } else {
+                        outputFile = $"{outputFolder}\\{Path.GetFileName(fontFile)}";
+                    }
+                    string exe = "pyftsubset.exe";
+                    string args =
+                        $"\"{fontFile}\"" +
+                        $" --text-file=\"{tmpfile}\"" +
+                        $" --output-file=\"{outputFile}\"" +
+                        $" --name-languages=0x0409";
+
+                    if (index > -1) {
+                        // ttc / otc, multi track
+                        args += $" --font-number={index}";
+                    }
+
+                    subsetFonts.Add(new SubsetFontInfo {
+                        FontNameInAss = fontName,
+                        OriginalFontFile = fontFile,
+                        SubsetFontFile = outputFile,
+                        SubsetFontName = randomName,
+                        TrackIndex = index
+                    });
+
+                    this.StartProcess(exe, args);
+                }
+            }
+
+            foreach (var font in subsetFonts) {
+                string exe = "ttx.exe";
+                string args = $"-f \"{font.SubsetFontFile}\"";
+
+                this.StartProcess(exe, args);
+            }
+
+            foreach (var font in subsetFonts) {
+                var ttxFile = Path.GetDirectoryName(font.SubsetFontFile) + "\\" + Path.GetFileNameWithoutExtension(font.SubsetFontFile) + ".ttx";
+                string ttxString = string.Empty;
+                using (StreamReader sr = new StreamReader(ttxFile, new UTF8Encoding(false))) {
+                    ttxString = sr.ReadToEnd();
+                }
+
+                XmlDocument xd = new XmlDocument();
+                xd.LoadXml(ttxString);
+                XmlNodeList namerecordList = xd.SelectNodes("ttFont/name/namerecord");
+
+                string fontFamilyName = string.Empty;
+                string uniqueFontIdentifier = string.Empty;
+                string fullFontName = string.Empty;
+                string postScriptName = string.Empty;
+
+                foreach (XmlNode record in namerecordList) {
+                    string nameID = record.Attributes["nameID"].Value;
+                    string langID = record.Attributes["langID"].Value;
+                    if (langID != "0x409") {
+                        continue;
+                    }
+
+                    switch (nameID) {
+                        case "1":
+                        case "3":
+                        case "4":
+                        case "6":
+                            record.FirstChild.Value = font.SubsetFontName;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                xd.Save(ttxFile);
+
+                string exe = "ttx.exe";
+                string args = $"-f \"{ttxFile}\"";
+                this.StartProcess(exe, args);
+
+                File.Delete(ttxFile);
+            }
+
+            foreach (string assFile in assFiles) {
+                List<string> assContent = new List<string>();
+                using (StreamReader sr = new StreamReader(assFile, true)) {
+                    while (!sr.EndOfStream) {
+                        assContent.Add(sr.ReadLine());
+                    }
+                }
+
+                int index = assContent.FindIndex(row => row.Length >= 13 && row.Substring(0, 13).ToLower() == "[script info]");
+
+                foreach (var kv in fonts) {
+                    string fontName = kv.Key;
+                    var subsetFontInfo = subsetFonts.Find(f => f.FontNameInAss == fontName);
+
+                    foreach (var _ in kv.Value) {
+                        if (_.Item1 != assFile) {
+                            continue;
+                        }
+                        int line = _.Item2;
+
+                        string row = assContent[line];
+                        if (row.Substring(0, 6).ToLower() == "style:") {
+                            assContent[line] = row.Replace(fontName, subsetFontInfo.SubsetFontName);
+                        } else if (row.Substring(0, 9).ToLower() == "dialogue:") {
+                            assContent[line] = row.Replace($@"\fn{fontName}", $@"\fn{subsetFontInfo.SubsetFontName}");
+                        }
+                    }
+
+                    string replaceComment = $"; Font Subset: {subsetFontInfo.SubsetFontName} - {fontName}";
+                    if (!assContent.Contains(replaceComment)) {
+                        assContent.Insert(++index, replaceComment);
+                    }
+                }
+
+
+                string newAssContent = string.Join("\r\n", assContent);
+                using (StreamWriter sw = new StreamWriter(outputFolder + "\\" + Path.GetFileName(assFile), false, Encoding.UTF8)) {
+                    sw.Write(newAssContent);
+                }
+            }
+
+
+
+        }
+
+        private void StartProcess(string exe, string args)
+        {
+            Console.WriteLine($"{exe} {args}");
+
+            Process p = new Process {
+                StartInfo = new ProcessStartInfo() {
+                    FileName = exe,
+                    Arguments = args,
+                    UseShellExecute = false
+                }
+            };
+            p.Start();
+            p.WaitForExit();
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        public Random random = new Random();
+        public string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) {
+                e.Effect = DragDropEffects.Copy;
+            } else {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] assFile = (string[])e.Data.GetData(DataFormats.FileDrop);
+            this.listBox1.Items.AddRange(assFile);
+            this.textBox2.Text = Path.GetDirectoryName(assFile[0]) + "\\fonts";
+            this.textBox3.Text = Path.GetDirectoryName(assFile[0]) + "\\output";
+
+            this.textBox2.Select(this.textBox2.Text.Length - 1, 0);
+            this.textBox3.Select(this.textBox3.Text.Length - 1, 0);
+        }
+    }
+
+}
